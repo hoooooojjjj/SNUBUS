f0c801f0ce98081c188a1fd1d7a1b8acbf4ea69f
sono <rhj080471@gmail.com>
2024-05-05 01:00:44 +0900

    Fix : CORS 에러 해결(http-proxy-middleware)

--- snubus/src/App.js
+++ snubus/src/App.js
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    getData();
+  }, []);
+  async function getData() {
+    try {
+      const response = await axios.get(
+        `bus_api?ServiceKey=${process.env.REACT_APP_BUS_API_KEY}&busRouteId=100100118&startOrd=1&endOrd=13&resultType=json`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return <div></div>;
 }
 
---
+++ snubus/src/setupProxy.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/bus_api",
+    createProxyMiddleware({
+      target: "http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid",
+      pathRewrite: {
+        // bus_api로 시작되는 url을 자동 인식 -> 프록시 처리, /bus_api는 ""로 대체됨
+        "^/bus_api": "",
+      },
+      changeOrigin: true,
+    })
+  );
+};
