76ec8c4b74329a380a2fb8130a986f692032a304
sono <rhj080471@gmail.com>
2024-05-07 20:55:58 +0900

    Fix : CORS 에러 해결 !! 버스 위치 데이터 받아오기 성공!

--- snubus/src/App.js
+++ snubus/src/App.js
@@ -3,18 +3,32 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
+    console.log(process.env.REACT_APP_BUS_API_KEY);
     getData();
   }, []);
-  async function getData() {
-    try {
-      const response = await axios.get(
-        `bus_api?ServiceKey=${process.env.REACT_APP_BUS_API_KEY}&busRouteId=100100118&startOrd=1&endOrd=13&resultType=json`
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+  // async function getData() {
+  //   try {
+  //     const response = await axios.get(
+  //       ``
+  //     );
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+  function getData() {
+    fetch(
+      `https://api.allorigins.win/get?url=${encodeURIComponent(
+        `http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?ServiceKey=${process.env.REACT_APP_BUS_API_KEY}&busRouteId=100100118&startOrd=1&endOrd=2&resultType=json`
+      )}`
+    )
+      .then((response) => {
+        if (response.ok) return response.json();
+        throw new Error("Network response was not ok.");
+      })
+      .then((data) => console.log(JSON.parse(data.contents)));
   }
+
   return <div></div>;
 }
 
--- snubus/src/setupProxy.js
+++ snubus/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/bus_api",
     createProxyMiddleware({
-      target: "http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid",
+      target: "http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt",
       pathRewrite: {
         // bus_api로 시작되는 url을 자동 인식 -> 프록시 처리, /bus_api는 ""로 대체됨
         "^/bus_api": "",
