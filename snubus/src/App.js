8f89175f265835211b1de74c4525425ebcf5ce96
sono <rhj080471@gmail.com>
2024-05-09 00:45:22 +0900

    Feat : 버스 위치 데이터 받아와서 마커로 만들기 성공 - 실시간 데이터 반영 미지수

--- snubus/src/App.js
+++ snubus/src/App.js
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
 import Map from "./util/Map";
 function App() {
+  // 현재 버스 위치 상태
+  const [position, setPosition] = useState([]);
+
+  // 버스 위치 정보 fetching
+  setInterval(() => {
+    getData();
+  }, 5000);
+
+  // 버스 위치 정보 fetching
+  async function getData() {
+    const response = await fetch(
+      `https://api.allorigins.win/get?url=${encodeURIComponent(
+        `http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?ServiceKey=${process.env.REACT_APP_BUS_API_KEY}&busRouteId=100100250&resultType=json`
+      )}`
+    );
+    const jsonData = await response.json();
+    console.log(
+      JSON.parse(jsonData.contents).msgBody.itemList[0].gpsY,
+      JSON.parse(jsonData.contents).msgBody.itemList[0].gpsX
+    );
+    const gpsY = JSON.parse(jsonData.contents).msgBody.itemList[0].gpsY;
+    const gpsX = JSON.parse(jsonData.contents).msgBody.itemList[0].gpsX;
+
+    setPosition((prev) => [gpsY, gpsX]);
+  }
   return (
     <div>
-      <Map></Map>
+      <Map position={position}></Map>
     </div>
   );
 }
--- snubus/src/util/Map.js
+++ snubus/src/util/Map.js
@@ -1,37 +1,13 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
-function Map() {
-  // 현재 버스 위치 상태
-  const [position, setPosition] = useState([]);
-
+function Map({ position }) {
   // kakaomap이 있는 요소의 ref
   const kakaoMap = useRef();
 
   useEffect(() => {
-    // 버스 위치 정보 fetching
-    getData();
     // 현재 위치 좌표 가져오기
     getCurrentPosition(printKakaomap);
-  }, []);
-
-  // 버스 위치 정보 fetching
-  function getData() {
-    fetch(
-      `https://api.allorigins.win/get?url=${encodeURIComponent(
-        `http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?ServiceKey=${process.env.REACT_APP_BUS_API_KEY}&busRouteId=100100118&resultType=json`
-      )}`
-    )
-      .then((response) => {
-        if (response.ok) return response.json();
-        throw new Error("Network response was not ok.");
-      })
-      .then((data) => {
-        // 현재 버스의 WGS84 좌표
-        const posX = JSON.parse(data.contents).msgBody.itemList[0].posX;
-        const posY = JSON.parse(data.contents).msgBody.itemList[0].posY;
-        setPosition((prev) => [posX, posY]);
-      });
-  }
+  }, [position]);
 
   // 현재 위치 좌표 가져오기
   const getCurrentPosition = (printKakaomap) => {
@@ -39,10 +15,10 @@ function Map() {
     if (navigator.geolocation) {
       // GeoLocation을 이용해서 접속 위치를 얻어옴
       navigator.geolocation.getCurrentPosition(function (position) {
-        const lat = position.coords.latitude, // 위도
-          lon = position.coords.longitude; // 경도
+        const curLat = position.coords.latitude, // 위도
+          curLlon = position.coords.longitude; // 경도
         // 카카오맵 그리기
-        printKakaomap(lat, lon);
+        printKakaomap(curLat, curLlon);
       });
     } else {
       // HTML5의 GeoLocation을 사용할 수 없을때
@@ -50,31 +26,47 @@ function Map() {
   };
 
   // 카카오맵 그리기
-  function printKakaomap(lat, lon) {
+  function printKakaomap(curLat, curLlon) {
     // ref가 kakaoMap인 요소를 container에 넣기
     const container = kakaoMap.current; //지도를 담을 영역의 DOM 레퍼런스
     // 지도를 생성할 때 필요한 기본 옵션
     const options = {
-      center: new window.kakao.maps.LatLng(lat, lon), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
+      center: new window.kakao.maps.LatLng(curLat, curLlon), //지도의 중심좌표.
+      level: 6, //지도의 레벨(확대, 축소 정도)
     };
     const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-    printMarker(map, lat, lon);
+    // 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+    window.kakao.maps.event.addListener(map, "center_changed", function () {
+      // 지도의  레벨을 얻어옵니다
+      var level = map.getLevel();
+      // 지도의 중심좌표를 얻어옵니다
+      var latlng = map.getCenter();
+    });
+
+    printMarker(map, curLat, curLlon);
   }
 
-  // 현재 위치 마커 생성
-  const printMarker = (map, lat, lon) => {
+  // 마커 생성
+  const printMarker = (map, curLat, curLlon) => {
     const clusterer = new window.kakao.maps.MarkerClusterer({
       map: map,
       gridSize: 35,
     });
 
-    const marker = new window.kakao.maps.Marker({
-      position: new window.kakao.maps.LatLng(lat, lon),
+    const curMarker = new window.kakao.maps.Marker({
+      position: new window.kakao.maps.LatLng(curLat, curLlon),
     });
 
-    clusterer.addMarker(marker);
+    const busMarker = new window.kakao.maps.Marker({
+      position: new window.kakao.maps.LatLng(
+        Number(position[0]),
+        Number(position[1])
+      ),
+    });
+    const markers = [curMarker, busMarker];
+    clusterer.addMarker(curMarker);
+    clusterer.addMarker(busMarker);
   };
 
   return <div ref={kakaoMap} style={{ width: "500px", height: "400px" }}></div>;
