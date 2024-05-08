8c5d5454c199c057c2f52964c126fb59c76d7524
sono <rhj080471@gmail.com>
2024-05-08 23:22:38 +0900

    Feat : 마커 클러스터 생성

--- snubus/src/util/Map.js
+++ snubus/src/util/Map.js
@@ -3,15 +3,15 @@ import React, { useEffect, useState, useRef } from "react";
 function Map() {
   // 현재 버스 위치 상태
   const [position, setPosition] = useState([]);
+
   // kakaomap이 있는 요소의 ref
   const kakaoMap = useRef();
 
   useEffect(() => {
-    console.log(window.kakao);
     // 버스 위치 정보 fetching
     getData();
-    // 카카오맵 그리기
-    printKakaomap();
+    // 현재 위치 좌표 가져오기
+    getCurrentPosition(printKakaomap);
   }, []);
 
   // 버스 위치 정보 fetching
@@ -33,20 +33,49 @@ function Map() {
       });
   }
 
+  // 현재 위치 좌표 가져오기
+  const getCurrentPosition = (printKakaomap) => {
+    // HTML5의 geolocation으로 사용할 수 있는지 확인
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옴
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const lat = position.coords.latitude, // 위도
+          lon = position.coords.longitude; // 경도
+        // 카카오맵 그리기
+        printKakaomap(lat, lon);
+      });
+    } else {
+      // HTML5의 GeoLocation을 사용할 수 없을때
+    }
+  };
+
   // 카카오맵 그리기
-  function printKakaomap() {
+  function printKakaomap(lat, lon) {
     // ref가 kakaoMap인 요소를 container에 넣기
     const container = kakaoMap.current; //지도를 담을 영역의 DOM 레퍼런스
     // 지도를 생성할 때 필요한 기본 옵션
-    let options = {
-      center: new window.kakao.maps.LatLng(
-        37.365264512305174,
-        127.10676860117488
-      ), //지도의 중심좌표.
+    const options = {
+      center: new window.kakao.maps.LatLng(lat, lon), //지도의 중심좌표.
       level: 3, //지도의 레벨(확대, 축소 정도)
     };
-    var map = window.kakao && new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+    printMarker(map, lat, lon);
   }
+
+  // 현재 위치 마커 생성
+  const printMarker = (map, lat, lon) => {
+    const clusterer = new window.kakao.maps.MarkerClusterer({
+      map: map,
+      gridSize: 35,
+    });
+
+    const marker = new window.kakao.maps.Marker({
+      position: new window.kakao.maps.LatLng(lat, lon),
+    });
+
+    clusterer.addMarker(marker);
+  };
 
   return <div ref={kakaoMap} style={{ width: "500px", height: "400px" }}></div>;
 }
