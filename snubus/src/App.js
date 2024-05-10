9ffe2021e473aed2e25028d262dd883ce7b0d06f
sono <rhj080471@gmail.com>
2024-05-11 01:12:16 +0900

    Refactor : 주석 추가, 여러 마커 생성 코드 축약

--- snubus/src/App.js
+++ snubus/src/App.js
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import Map from "./util/Map";
 
 function App() {
+  // 버스 데이터 저장하는 상태
   const [data, setData] = useState(null);
 
+  // 버스 위치 정보 데이터 fetching
   async function getData() {
     const response = await fetch(
       `https://api.allorigins.win/get?url=${encodeURIComponent(
@@ -14,24 +16,31 @@ function App() {
       }
     );
     const jsonData = await response.json();
+    // 버스 데이터 저장하는 상태에 저장
     setData(jsonData);
   }
 
+  // 데이터 fetching
   useEffect(() => {
+    // 처음 마운트 되었을 때 fetching
     getData();
+
+    // 10초 마다 데이터 업데이트
     const fetchDataInterval = setInterval(() => {
       getData();
     }, 10000); // 10초마다 데이터 다시 요청
 
     // 컴포넌트가 언마운트될 때 clearInterval 호출하여 메모리 누수 방지
     return () => clearInterval(fetchDataInterval);
-  }, []); // useEffect의 의존성 배열이 비어 있으므로 컴포넌트가 마운트될 때 한 번만 실행
+  }, []);
 
   return (
     <div>
       {!data ? (
+        // 데이터가 아직 안들어왔을 때
         <> </>
       ) : (
+        // 맵 컴포넌트에 데이터 전달
         <Map
           position={[
             JSON.parse(data.contents).msgBody.itemList[2].gpsY,
--- snubus/src/util/Map.js
+++ snubus/src/util/Map.js
@@ -11,6 +11,13 @@ function Map({ position }) {
     centerX: "",
   });
 
+  // 5511번 버스들 각각의 위치 좌표 배열
+  const busesPos = [
+    [37.6141, 126.910907],
+    [37.6142, 126.911007],
+    [37.6143, 126.91207],
+  ];
+
   useEffect(() => {
     // 현재 위치 좌표 가져오기
     getCurrentPosition(printKakaomap);
@@ -24,7 +31,7 @@ function Map({ position }) {
       navigator.geolocation.getCurrentPosition(function (position) {
         const curLat = position.coords.latitude, // 위도
           curLlon = position.coords.longitude; // 경도
-        // 카카오맵 그리기
+        // 카카오맵 그리기(현재 위치 위도, 경도 인자로)
         printKakaomap(curLat, curLlon);
       });
     } else {
@@ -32,56 +39,67 @@ function Map({ position }) {
     }
   };
 
-  // 카카오맵 그리기
+  // 카카오맵 그리기(현재 위치 위도, 경도 인자로)
   function printKakaomap(curLat, curLlon) {
     // ref가 kakaoMap인 요소를 container에 넣기
     const container = kakaoMap.current; //지도를 담을 영역의 DOM 레퍼런스
     // 지도를 생성할 때 필요한 기본 옵션
     const options = {
       center: new window.kakao.maps.LatLng(
-        mapInfo.centerY || Number(position[0]),
-        mapInfo.centerX || Number(position[1])
-      ), //지도의 중심좌표.
+        mapInfo.centerY || curLat,
+        mapInfo.centerX || curLlon
+      ), //지도의 중심좌표. -> 마운트 되기 전 map 확대 및 이동 위치가 있으면 그걸 중심좌표로 , 없으면 현재 위치를 중심좌표로
       level: mapInfo.level || 6, //지도의 레벨(확대, 축소 정도)
     };
     const map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-    // 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+    // 지도가 이동, 확대, 축소로 인해 중심좌표가 변경되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록
     window.kakao.maps.event.addListener(map, "center_changed", function () {
-      // 지도의  레벨을 얻어옵니다
+      // 사용자가 지정한 지도의 레벨, 좌표를 얻어옴
       let newMapInfo = {
         ...mapInfo,
         level: map.getLevel(),
         centerY: map.getCenter().Ma,
         centerX: map.getCenter().La,
       };
+      // 마운트 되기 전 map 확대 및 이동 위치
       setMapInfo(newMapInfo);
     });
-
+    // 마커 생성
     printMarker(map, curLat, curLlon);
   }
 
   // 마커 생성
   const printMarker = (map, curLat, curLlon) => {
+    // 마커 클러스터 만들기
     const clusterer = new window.kakao.maps.MarkerClusterer({
+      // 옵션
       map: map,
       gridSize: 35,
     });
 
+    // 현재 위치 마커 만들기
     const curMarker = new window.kakao.maps.Marker({
       position: new window.kakao.maps.LatLng(curLat, curLlon),
     });
 
-    console.log(Number(position[0]), Number(position[1]));
-    const busMarker = new window.kakao.maps.Marker({
-      position: new window.kakao.maps.LatLng(
-        Number(position[0]),
-        Number(position[1])
-      ),
-    });
-    const markers = [curMarker, busMarker];
+    // 5511번 버스들 위치 좌표 배열마다 마커 만들기
+    const markers = busesPos.map(
+      (busPos) =>
+        new window.kakao.maps.Marker({
+          position: new window.kakao.maps.LatLng(busPos[0], busPos[1]),
+        })
+    );
+
+    // 마커를 클러스터에 등록 -> 마커 print
+
+    // 현재 위치 마커 print
     clusterer.addMarker(curMarker);
-    clusterer.addMarker(busMarker);
+
+    // 5511번 버스들 위치 마커 print
+    markers.forEach((marker) => {
+      clusterer.addMarker(marker);
+    });
   };
 
   return <div ref={kakaoMap} style={{ width: "500px", height: "400px" }}></div>;
