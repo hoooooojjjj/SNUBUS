cc34b4f0fda5a98328cc5467f8f5edbf362ce582
sono <rhj080471@gmail.com>
2024-06-13 23:27:14 +0900

    Fix : 정류장 방면 전환 버튼 오류 수정

--- snubus/src/components/StationLine.js
+++ snubus/src/components/StationLine.js
@@ -12,19 +12,24 @@ const StationLine = () => {
   // 중앙대학교 방면을 선택했는지 확인하는 state(방면 전환)
   const [isStart, setIsStart] = useState(true);
 
-  // 각 방면 버튼을 클릭하면 정류장 라인 전환
-  const switchDirection = () => {
-    setIsStart(!isStart);
+  // 중앙대학교 방면 버튼을 클릭하면 중앙대학교 방면 정류장 라인으로 전환
+  const switchDirectionToStart = () => {
+    setIsStart(true);
+  };
+
+  // 신림2동차고지 방면 버튼을 클릭하면 신림2동차고지 방면 정류장 라인으로 전환
+  const switchDirectionToEnd = () => {
+    setIsStart(false);
   };
 
   return (
     <Container>
       <StationLineWrap>
         <BtnWrap>
-          <StationSwitchBtn onClick={switchDirection}>
+          <StationSwitchBtn onClick={switchDirectionToStart}>
             중앙대학교 방면
           </StationSwitchBtn>
-          <StationSwitchBtn onClick={switchDirection}>
+          <StationSwitchBtn onClick={switchDirectionToEnd}>
             신림2동차고지 방면
           </StationSwitchBtn>
         </BtnWrap>
