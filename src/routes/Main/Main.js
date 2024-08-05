1301add720330e6512cfbc2fc7102c83028d26f4
sono <rhj080471@gmail.com>
2024-08-05 17:08:56 +0900

    Refactor : Main 컴포넌트 (Main 페이지) 하는 일 나열

--- src/routes/Main/Main.js
+++ src/routes/Main/Main.js
@@ -7,6 +7,16 @@ import busInfo from "../../json/busInfo.json";
 import { useNavigate } from "react-router-dom";
 import { hasVisitedContext } from "../../App";
 
+/* - Main 컴포넌트 (Main 페이지)
+    1. 이 컴포넌트가 하는 일
+        1. 처음 방문인지 아닌지 판단 후 처음 방문이면 intro 페이지로 이동
+            1. Hooks 기능으로 분리 → useHasVisited() 커스텀 훅으로 분리
+        2. Main 화면 렌더링(하위 : Headers, Articles, Footers 컴포넌트)
+            1. Component 기능으로 분리 → Main 컴포넌트
+        3. 배경이미지를 변경하는 imgNum state를 전달하는 context API를 만들고, Navigation 컴포넌트에 전달해 해당 컴포넌트에서 배경이미지를 변경할 때 배경이미지  변경
+            1. Reducer 기능으로 분리 → Header - Navigation 컴포넌트로 분리 
+*/
+
 // 처음 방문인지 아닌지 판단하는 커스텀 훅
 function useHasVisited() {
   const nav = useNavigate();
