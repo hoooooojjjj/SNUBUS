8f9653100b14c5dd6d0bffa33de02981e6b0b26c
sono <rhj080471@gmail.com>
2024-08-05 18:28:34 +0900

    Refactor : Articles 컴포넌트 단일 책임 원칙 적용
            1. 해당 버스 이름 헤더 렌더링
                1. Component 기능으로 분리 →  ArticleHeader 컴포넌트
            2. 해당 버스 노선들 전처리 후 렌더링
                1. Component 기능으로 분리 →  BusRouteList 컴포넌트
            3. 해당 버스 노선 선택할 수 있는 슬라이드 렌더링
                1. Component 기능으로 분리 →  Slide 컴포넌트 */

--- src/routes/Main/components/Articles/Article.js
+++ src/routes/Main/components/Articles/Article.js
@@ -1,27 +1,84 @@
-import React, { useState } from "react";
-import { Article, H2, Img, HeaderWrap, P } from "./ArticleStyle";
-import Slide from "../../../components/Slide";
+import React from "react";
+import {
+  Article,
+  H2,
+  Img,
+  HeaderWrap,
+  P,
+  StyledCarousel,
+  CarouselWrap,
+  SlideItem,
+  SlideText,
+} from "./ArticleStyle";
+import { useNavigate } from "react-router-dom";
 
-function Articles({ info }) {
-  const [showOver4, setShowOver4] = useState(false);
+/* - Articles 컴포넌트 (Headers 컴포넌트  하위)
+    - 이 컴포넌트가 하는 일
+        1. 해당 버스 이름 헤더 렌더링
+            1. Component 기능으로 분리 →  ArticleHeader 컴포넌트
+        2. 해당 버스 노선들 전처리 후 렌더링
+            1. Component 기능으로 분리 →  BusRouteList 컴포넌트
+        3. 해당 버스 노선 선택할 수 있는 슬라이드 렌더링
+            1. Component 기능으로 분리 →  Slide 컴포넌트 */
 
-  const onClick = () => {
-    setShowOver4(!showOver4);
+// ArticleHeader 컴포넌트
+function ArticleHeader({ info }) {
+  return (
+    <HeaderWrap>
+      <Img src={process.env.PUBLIC_URL + `assets/${info.id}.svg`} />
+      <H2>{info.name}</H2>
+    </HeaderWrap>
+  );
+}
+
+// 해당 버스 노선 리스트 컴포넌트
+function BusRouteList({ info }) {
+  // 버스 리스트 전처리 함수
+  const PreprocessingBusList = () => {
+    // 버스 리스트를 ,로 구분하여 출력
+    const busList = info.buslist.map((bus, i) => {
+      const PreprocessedbusList =
+        bus.num + (i !== parseInt(info.buslist.length) - 1 ? ", " : "");
+
+      return <span key={bus.num}>{PreprocessedbusList}</span>;
+    });
+
+    return busList;
+  };
+
+  return <P>{PreprocessingBusList()}</P>;
+}
+
+// 해당 버스 노선 선택할 수 있는 슬라이드 컴포넌트
+function Slide({ info }) {
+  const nav = useNavigate();
+
+  // 해당 버스 노선 클릭 시 해당 노선 페이지로 이동
+  const onBusNumClick = (bus) => {
+    nav(`/view/${bus.num}`);
   };
 
   return (
-    <Article id={info.id}>
-      <HeaderWrap>
-        <Img src={process.env.PUBLIC_URL + `assets/${info.id}.svg`} />
-        <H2>{info.name}</H2>
-      </HeaderWrap>
-      <P>
-        {info.buslist.map((bus, i) => (
-          <span key={bus.num} onClick={onClick}>
-            {bus.num + (i !== parseInt(info.buslist.length) - 1 ? ", " : "")}
-          </span>
+    <CarouselWrap>
+      <StyledCarousel arrows={true}>
+        {info.buslist.map((bus) => (
+          <SlideItem key={bus.num}>
+            <SlideText onClick={() => onBusNumClick(bus)}>
+              {bus.num}번 버스
+            </SlideText>
+          </SlideItem>
         ))}
-      </P>
+      </StyledCarousel>
+    </CarouselWrap>
+  );
+}
+
+// Articles 컴포넌트(Main 컴포넌트 하위)
+function Articles({ info }) {
+  return (
+    <Article>
+      <ArticleHeader info={info} />
+      <BusRouteList info={info} />
       <Slide info={info} />
     </Article>
   );
--- src/routes/Main/components/Articles/ArticleStyle.js
+++ src/routes/Main/components/Articles/ArticleStyle.js
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { Font_DoHyeon, flex } from "../../../../util/publicStyleComponents";
+import { Carousel } from "antd";
 
 // article wrap
 export const Article = styled.article({
@@ -90,6 +91,76 @@ export const P = styled.p({
     marginBottom: 0,
   },
   "@media(max-width: 320px)": {
+    fontSize: 25,
+  },
+});
+
+// Slide
+const SlideItemHeight = "120px";
+const SlideItemHeight_under485 = "100px";
+const SlideItemHeight_under550 = "90px";
+
+// Carousel wrap
+export const CarouselWrap = styled.header({
+  width: "100dvw",
+  height: "35%",
+  "@media (max-width: 550px)": {
+    height: "30%",
+  },
+
+  background:
+    "radial-gradient(circle, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0.7) 50%, rgba(0, 0, 0, 0.2) 70%)",
+});
+
+export const StyledCarousel = styled(Carousel)`
+  width: 40%;
+  margin: 0 auto;
+  @media (max-width: 550px) {
+    width: 85%;
+  }
+`;
+
+// 슬라이드 컴포넌트
+export const SlideItem = styled.div({
+  height: SlideItemHeight,
+  "@media(max-width: 485px)": {
+    height: SlideItemHeight_under485,
+  },
+  "@media(max-width: 550px)": {
+    height: SlideItemHeight_under550,
+  },
+
+  borderRadius: 20,
+});
+
+// 슬라이드 안에 텍스트 컴포넌트
+export const SlideText = styled.p({
+  ...flex("column", "center", "center"),
+  margin: "0 auto",
+  width: "70%",
+  lineHeight: SlideItemHeight,
+  ...Font_DoHyeon(),
+  fontSize: 40,
+  cursor: "pointer",
+  "@media(max-width: 768px)": {
+    fontSize: 35,
+  },
+  "@media(max-width: 650px)": {
+    fontSize: 32,
+  },
+  "@media(max-width: 595px)": {
+    fontSize: 25,
+  },
+  "@media(max-width: 485px)": {
+    fontSize: 22,
+    lineHeight: SlideItemHeight_under485,
+  },
+  "@media(max-width: 550px)": {
+    fontSize: 30,
+    lineHeight: SlideItemHeight_under550,
+  },
+  "@media(max-width: 320px)": {
+    lineHeight: SlideItemHeight_under550,
     fontSize: 25,
   },
 });
