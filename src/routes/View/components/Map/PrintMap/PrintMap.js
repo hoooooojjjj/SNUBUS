34955c806609feb7ed9f3c1c6e3e83fc2faafa32
sono <rhj080471@gmail.com>
2024-08-09 14:30:20 +0900

    Feat : 버스 정류장 클릭 후 리렌더링될 때 무조건 클릭한 정류장 좌표로 중심좌표가 돌아가는 문제 해결

--- src/routes/View/components/Map/PrintMap/PrintMap.js
+++ src/routes/View/components/Map/PrintMap/PrintMap.js
@@ -119,8 +119,6 @@ const printMarker = (
 
   // 정류장을 클릭했다면(busStationPos에 데이터가 할당되었다면)
   if (busStationPos.name) {
-    // !이렇게 하면 리렌더링될 때 무조건 클릭한 정류장 좌표로 중심좌표가 돌아가는 문제 생김!
-
     // 정류장을 클릭하면 클릭한 정류장 좌표로 중심좌표 이동
     map.setCenter(
       new window.kakao.maps.LatLng(busStationPos.pos[0], busStationPos.pos[1])
@@ -199,6 +197,8 @@ const printMarker = (
       );
     }
   }
+
+  busStationPos.name = "";
 
   // 각 방면마다
   if (isPolylinVisible.isStart) {
